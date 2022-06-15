9a55153e83be17b51c56462ef0a7e505cc4ccd71
nh-nayeem <95094210+nh-nayeem@users.noreply.github.com>
2022-06-16 01:32:28 +0600

    Update KMP Fail + Z-Algo.cpp

--- String/KMP Fail + Z-Algo.cpp
+++ String/KMP Fail + Z-Algo.cpp
@@ -1,44 +1,43 @@
 //0-indexed string s[0...n-1]
 //fail[i] = maximum x such that s[0....x-1] matches with s[i-x+1...i]
-int fail[MAX];
-void build_failure(string s){
-    int n=s.size();
-    for(int i=1;i<n;i++){
-        int j=fail[i-1];
-        while(j>0 && s[i]!=s[j]) j=fail[j-1];
-        if(s[i]==s[j]) j++;
-        fail[i]=j;
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+void compute_automaton(string s, vector<vector<int>>& aut) {
+    s += '#';
+    int n = s.size();
+    vector<int> pi = prefix_function(s);
+    aut.assign(n, vector<int>(26));
+    for (int i = 0; i < n; i++) {
+        for (int c = 0; c < 26; c++) {
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[pi[i-1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
+        }
     }
 }
 
-int KMPSearch(string txt, string pat){
-    int Count = 0;
-    int i = 0;
-    int j = 0;
-    while(i < txt.size()){
-        if(txt[i] == pat[j]) i++, j++;
-
-        if(j == pat.size()) Count++, j = fail[j - 1];
-        else if(i < txt.size() && txt[i] != pat[j]){
-            if(j != 0) j = fail[j - 1];
-            else i++;
-        }
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
     }
-    return Count;
-}
-
-//0-indexed string s[0...n-1]
-//z[i] = maximum x such that s[0....x-1] matches with s[i..i+x-1]
-int z[MAX];
-void zAlgo(string s){
-    int L=0,R=0;
-    int n=s.size();
-    for(int i=1;i<n;i++){
-        if(i>R) {L=R=i; while(R<n && s[R-L]==s[R]) R++; z[i]=R-L; R--;}
-        else{
-            int k=i-L;
-            if(z[k]<R-i+1) z[i]=z[k];
-            else {L=i; while(R<n && s[R-L]==s[R]) R++; z[i]=R-L; R--;}
-        }
-    }
+    return z;
 }
