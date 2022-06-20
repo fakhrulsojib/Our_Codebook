cd97cec58288e60182d6ef31d01014f518ddf8d2
Md Fakhrul Islam <fakhrulsojib@gmail.com>
2022-06-20 22:13:49 +0600

    Update sosdp.cpp

--- dp/sosdp.cpp
+++ dp/sosdp.cpp
@@ -1,11 +1,15 @@
-//SOS DP
+//iterative version
+for(int mask = 0; mask < (1<<N); ++mask){
+	dp[mask][-1] = A[mask];	//handle base case separately (leaf states)
+	for(int i = 0;i < N; ++i){
+		if(mask & (1<<i)) dp[mask][i] = dp[mask][i-1] + dp[mask^(1<<i)][i-1];
+		else              dp[mask][i] = dp[mask][i-1];
+	}
+	F[mask] = dp[mask][N-1];
+}
 
-for(int mask = 0; mask < (1<<N); mask++){
-    //handle base case separately (leaf states)
-    dp[mask][-1] = A[mask];
-    for(int b = 0; b < N; b++){
-        if(mask & (1<<b)) dp[mask][b] = dp[mask][b-1] + dp[mask^(1<<b)][b-1];
-        else              dp[mask][b] = dp[mask][b-1];
-    }
-    F[mask] = dp[mask][N-1];
+//memory optimized, super easy to code.
+for(int i = 0; i<(1<<N); ++i) F[i] = A[i];
+for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+	if(mask & (1<<i)) F[mask] += F[mask^(1<<i)];
 }
