0b3b807d4b3db7ea3d1954c75a1a252156b30aa5
nh-nayeem <95094210+nh-nayeem@users.noreply.github.com>
2022-08-31 16:56:57 +0600

    Update MatrixExponent.cpp

--- dp/MatrixExponent.cpp
+++ dp/MatrixExponent.cpp
@@ -1,34 +1,40 @@
 const int mod = 1e9 + 7;
-int add(int x,int y) {	return (x+y)%mod;  		}
-int mul(int x,int y) {	return (x*1ll*y)%mod;	}
-vector<vector<int> >  matmal(vector<vector<int> > a,vector<vector<int> > b)
+typedef long long ll;
+typedef vector<vector<ll> > matrix;
+matrix getmat(int r, int c, ll val = 0)
 {
-    int r1=a.size(),c1=a[0].size(),r2=b.size(),c2=b[0].size();
-    assert(c1==r2);
-    vector<vector<int> > ret(r1,vector<int> (c2,0));
-    for(int i=0;i<r1;i++)
+    vector<vector<ll> > ret(r, vector<ll>(c, val));
+    return ret;
+}
+matrix Identity(int r)
+{
+    auto ret = getmat(r, r);
+    for (int i = 0; i < r; i++) ret[i][i] = 1;
+    return ret;
+}
+matrix matmal(matrix x, matrix y)
+{
+    int r1 = x.size(), c1 = x[0].size(), r2 = y.size(), c2 = y[0].size();
+    assert(c1 == r2);
+    auto  ret = getmat(r1, c2);
+    for (int i = 0; i < r1; i++)
     {
-        for(int j=0;j<c2;j++)
+        for (int j = 0; j < c2; j++)
         {
-            for(int k=0;k<c1;k++)
+            for (int k = 0; k < r2; k++)
             {
-                ret[i][j]=add(ret[i][j],mul(a[i][k],b[k][j]));
+                ret[i][j] += (x[i][k] * y[k][j]) % mod;
+                ret[i][j] %= mod;
             }
         }
     }
     return ret;
 }
-vector<vector<int> > identity(int n)
+matrix bigmod(matrix x, ll b)
 {
-    vector<vector<int> > ret(n,vector<int>(n,0));
-    for(int i=0;i<n;i++) ret[i][i]=1;
-    return ret;
-}
-vector<vector<int> >  bigpow(vector<vector<int> > a,int b)
-{
-    if(b==0) return identity(a.size());
-    auto ret=bigpow(a,b>>1);
-    ret=matmal(ret,ret);
-    if(b&1) ret=matmal(ret,a);
+    if (b == 0) return Identity(x.size());
+    auto ret = bigmod(x, b >> 1);
+    ret = matmal(ret, ret);
+    if (b & 1) ret = matmal(ret, x);
     return ret;
 }
